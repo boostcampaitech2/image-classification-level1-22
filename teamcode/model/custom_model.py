4c8b827bd4ded083d650100ae448d38a5b16132e
promisemee <promise_mee@daum.net>
2021-09-02 08:11:30 +0000

    add custom functions

--- teamcode/model/custom_model.py
+++ teamcode/model/custom_model.py
@@ -35,3 +35,25 @@ class CustomVit(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+class CustomEfficientNetWithClassifier(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.model_name = 'efficientnet-b0'
+        self.num_classes = num_classes
+
+        self.model = timm.create_model('efficientnet_b0', num_classes=num_classes, pretrained=True)
+
+        self.classifier = nn.Sequential(
+            nn.Linear(1000, 256),
+            nn.Dropout(p=0.2),
+            nn.ReLU(),
+            nn.Linear(256, self.num_classes),
+        )
+        
+    def forward(self, x):
+        x = self.model(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        
+        return x
--- teamcode/util/custom_util.py
+++ teamcode/util/custom_util.py
@@ -1,5 +1,38 @@
 from pytz import timezone
 from datetime import datetime as dt
+import numpy as np
+import torch
 
 def get_now_str():
     return dt.now().astimezone(timezone("Asia/Seoul")).strftime('%Y%m%d%H%M%S')
+
+def get_label_count(dataset, num_classes):
+    labels = torch.zeros(num_classes, dtype=torch.long)
+
+    for _, target in dataset:
+        labels[target] += 1
+
+    return labels
+
+def get_loss_weight(class_list, weight_schemes = "ENS", beta = 0.99, num_classes=18):
+    label_count = class_list
+    if torch.is_tensor(label_count): label_count = label_count.tolist()
+
+    if weight_schemes == "INS":
+        loss_weight = 1 / label_count
+
+    elif weight_schemes == "ISNS":
+        loss_weight = 1 / label_count
+    
+    elif weight_schemes == "ENS":
+        eff_num = (1.0-np.power(beta, label_count))
+        weights = (1.0-beta) / eff_num
+        loss_weight = weights /np.sum(weights) * num_classes
+
+    elif weight_schemes == "MAX":
+        label_max = label_count.max()
+        loss_weight = label_max / label_count
+
+    loss_weight = torch.tensor(loss_weight).float()
+
+    return loss_weight
