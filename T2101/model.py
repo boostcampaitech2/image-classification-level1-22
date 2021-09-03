660c56e31faa69926dd82b5b6903acd258ca53af
bagineer <bih198721@gmail.com>
2021-09-03 11:21:23 +0000

    add private script files and submission file

--- T2101/model.py
+++ T2101/model.py
@@ -2,9 +2,11 @@ import torch.nn as nn
 import timm
 
 class MyModel(nn.Module):
-    def __init__(self):
+    def __init__(self, pretrained=True):
         super().__init__()
-        self.model = timm.create_model('efficientnet_b0', num_classes=18, pretrained=True)
+        self.name = 'efficientnet_b0'
+        self.model = timm.create_model(self.name, num_classes=18, pretrained=pretrained)
+        self.name += f'_{pretrained}'
 
     def forward(self, X):
         return self.model(X)
